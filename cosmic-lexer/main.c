d3be836b130d8f55c9e8d827c1335c595ba6a574
Kurt M. Weber <weberk294@gmail.com>
2024-02-25 12:16:52 -0600

    add cosmic-error header, make infrastructure

--- cosmic-lexer/main.c
+++ cosmic-lexer/main.c
@@ -1,5 +1,5 @@
 /*****************************************************************
- * This file is part of CosmOS                                   *
+ * This file is part of CosmiC                                   *
  * Copyright (C) 2024 Kurt M. Weber                              *
  * Released under the stated terms in the file LICENSE           *
  * See the file "LICENSE" in the source distribution for details *
---
+++ include/cosmic-error.h
@@ -0,0 +1,11 @@
+/*****************************************************************
+ * This file is part of CosmiC                                   *
+ * Copyright (C) 2024 Kurt M. Weber                              *
+ * Released under the stated terms in the file LICENSE           *
+ * See the file "LICENSE" in the source distribution for details *
+ *****************************************************************/
+
+#ifndef _COSMIC_ERROR_H
+#define _COSMIC_ERROR_H
+
+#endif
